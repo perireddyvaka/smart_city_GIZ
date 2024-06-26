2847808113555018f59cdda4cee4f57e37c3e6e4
ushasrimogadali123 <ushasrimogadali123@gmail.com>
2024-06-26 14:49:52 +0530

    commiting docker

--- Server/db.js
+++ Server/db.js
@@ -1,12 +1,33 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const client = new Pool({
+// const client = new Pool({
+//   user: "postgres",
+//   host: "192.168.137.167",
+//   database: "bypldb",
+//   password: "1234",
+//   port: 5432, // Default PostgreSQL port
+// });
+
+
+// module.exports = client;
+
+// db.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
   user: "postgres",
-  host: "127.0.0.1",
+  host: "postgres",  // Service name of PostgreSQL container in Docker Compose
   database: "bypldb",
-  password: "Postgres",
-  port: 5432, // Default PostgreSQL port
+  password: "1234",
+  port: 5432,
+  idleTimeoutMillis: 200000,
+  connectionTimeoutMillis: 200000,
 });
 
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
 
-module.exports = client;
+// Export the pool instead of client
+module.exports = pool;
