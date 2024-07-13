7a3811dbabbcd8c89f0ab2c76d0cf9f76e89727c
Nanihack12 <vakaperireddy59@gmail.com>
2024-07-13 12:01:53 +0530

    config changes to localhost

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
     //backendAPI : "https://smartcitylivinglab.iiit.ac.in/zf-backend-api",
-    backendAPI: "http://192.168.56.101:4313",
+    backendAPI: "http://localhost:4313",
     // middlewareAPI: "http://smartcitylivinglab.iiit.ac.in:1630",
     // backendAPI: "http://localhost:1629",
     // middlewareAPI: "http://localhost:1630"
