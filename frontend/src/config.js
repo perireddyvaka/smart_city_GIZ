70b4ae6c45f5dd7e2095f02887b788d6f62e380f
Nanihack12 <vakaperireddy59@gmail.com>
2024-07-23 15:16:38 +0530

    routes commented for 3 pages and nginx fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,8 @@
 // config.js
 const config = {
-    //backendAPI : "https://smartcitylivinglab.iiit.ac.in/zf-backend-api",
+   
     backendAPI: "http://localhost:4313",
-    // middlewareAPI: "http://smartcitylivinglab.iiit.ac.in:1630",
-    // backendAPI: "http://localhost:1629",
-    // middlewareAPI: "http://localhost:1630"
+    
 };
 
 export default config;
